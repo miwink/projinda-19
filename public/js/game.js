2343447bd71cdad4c97788fb7a2e4405da24bf8f
miwink <miwink@kth.se>
2019-05-13 20:38:32 +0200

    Creating collisions between pacman and walls

--- public/js/game.js
+++ public/js/game.js
@@ -1,70 +1,93 @@
 //var Phaser = require("phaser");
 
 var config = {
-  type: Phaser.AUTO,
-  width: 800,
-  height: 600,
-  scene: {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    scene: {
     preload: preload,
     create: create,
     update: update
-  }
+  },
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 0},
+            enableBody: {}
+        }
+    }
+
 };
 
-var pacman;
 var cursors;
 var game = new Phaser.Game(config);
 
 function preload() {
-  this.load.tilemapTiledJSON("map", "../assets/pacman-map.json");
-  this.load.image("tiles", "../assets/tiles.png");
+    this.load.tilemapTiledJSON("map", "../assets/pacman-map.json");
 
-  this.load.image("pacman-tiles", "../assets/pacman-tiles.png");
-  this.load.spritesheet("pacman", "../assets/pacman.png", {
-    frameWidth: 32,
-    frameHeight: 32
-  });
-  // this.load.image("pacman", "../assets/pacman.png");
-  //this.load.image("dot", "../assets/dot.png");
+    this.load.image("pacman-tiles", "../assets/pacman-tiles.png");
+    this.load.spritesheet("pacman", "../assets/pacman.png", {
+        frameWidth: 32,
+        frameHeight: 32
+    });
+    this.load.image("dot", "../assets/dot.png");
 }
 
 function create() {
-  this.map = this.add.tilemap("map");
-  this.tileset = this.map.addTilesetImage("pacman-tiles", "pacman-tiles");
-  this.layer = this.map.createDynamicLayer("Pacman", this.tileset);
+    this.map = this.add.tilemap("map");
+    this.tileset = this.map.addTilesetImage("pacman-tiles", "pacman-tiles");
+    this.layer = this.map.createDynamicLayer("Pacman", this.tileset);
 
-  const pacman = (this.pacman = this.add.sprite(23, 23, "pacman", 0)).setOrigin(
-    0.5
-  );
-  this.anims.create({
-    key: "munch",
-    repeat: -1,
-    frameRate: 7,
-    frames: this.anims.generateFrameNames("pacman", { start: 0, end: 3 })
-  });
-  pacman.play("munch");
+    pacman = this.add.sprite(23, 23, "pacman", 0).setOrigin(0.5);
 
-  cursors = this.input.keyboard.createCursorKeys();
+    var config = {
+        key: 'dot',
+        x: 1,
+        y: 1
+    }; 
+
+    this.dots = this.map.createFromTiles(7, 14, config);
+
+    this.map.setCollisionByExclusion([14], true, false, this.layer);
+    //this.map.setCollisionBetween(0,13);
+
+    this.anims.create({
+        key: "munch",
+        repeat: -1,
+        frameRate: 7,
+        frames: this.anims.generateFrameNames("pacman", { start: 0, end: 3 })
+    });
+    pacman.play("munch");
+
+    cursors = this.input.keyboard.createCursorKeys();
 
   //this.dots = this.add.physicsGroup();
   //this.map.createFromTile(7, this.safetile, "dot", this.layer, this.dot);
+    this.physics.add.collider(pacman, this.layer);
+
+    const debugGraphics = this.add.graphics().setAlpha(0.75);
+    this.map.renderDebug(debugGraphics, {
+        tileColor: null, // Color of non-colliding tiles
+        collidingTileColor: new Phaser.Display.Color(243, 134, 48, 255), // Color of colliding tiles
+        faceColor: new Phaser.Display.Color(40, 39, 37, 255) // Color of colliding face edges
+    });
 }
 
 function update() {
-  if (cursors.up.isDown) {
-    this.pacman.y--;
-    this.pacman.angle = 270;
-  }
-  if (cursors.down.isDown) {
-    this.pacman.y++;
-    this.pacman.angle = 90;
-  }
-  if (cursors.left.isDown) {
-    this.pacman.x--;
-    this.pacman.angle = 180;
-  }
-  if (cursors.right.isDown) {
-    this.pacman.x++;
-    this.pacman.angle = 0;
-  }
+    if (cursors.up.isDown) {
+        pacman.y--;
+        pacman.angle = 270;
+    }
+    if (cursors.down.isDown) {
+        pacman.y++;
+        pacman.angle = 90;
+    }
+    if (cursors.left.isDown) {
+        pacman.x--;
+        pacman.angle = 180;
+    }
+    if (cursors.right.isDown) {
+        pacman.x++;
+        pacman.angle = 0;
+    }
 }
