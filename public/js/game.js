d09d53aee80b219a8b1cb3b01438ff07e0620a7a
miwink <miwink@kth.se>
2019-05-14 14:32:58 +0200

    Added wall detection so that pacman cant move through walls

--- public/js/game.js
+++ public/js/game.js
@@ -1,19 +1,18 @@
-//var Phaser = require("phaser");
-
 var config = {
     type: Phaser.AUTO,
     width: 800,
     height: 600,
     scene: {
-    preload: preload,
-    create: create,
-    update: update
-  },
+        preload: preload,
+        create: create,
+        update: update
+    },
     physics: {
         default: 'arcade',
         arcade: {
+            //debug: true,
             gravity: { y: 0},
-            enableBody: {}
+            //enableBody: {}
         }
     }
 
@@ -21,6 +20,7 @@ var config = {
 
 var cursors;
 var game = new Phaser.Game(config);
+this.pacman = null;
 
 function preload() {
     this.load.tilemapTiledJSON("map", "../assets/pacman-map.json");
@@ -38,15 +38,15 @@ function create() {
     this.tileset = this.map.addTilesetImage("pacman-tiles", "pacman-tiles");
     this.layer = this.map.createDynamicLayer("Pacman", this.tileset);
 
-    pacman = this.add.sprite(23, 23, "pacman", 0).setOrigin(0.5);
+    this.pacman = this.physics.add.sprite(23, 23, "pacman", 0);//.setOrigin(0.5);
 
-    var config = {
+    var configDot = {
         key: 'dot',
-        x: 1,
-        y: 1
+        x: 0.5,
+        y: 0.5,
     }; 
-
-    this.dots = this.map.createFromTiles(7, 14, config);
+    
+    this.dots = this.map.createFromTiles(7, 14, configDot);
 
     this.map.setCollisionByExclusion([14], true, false, this.layer);
     //this.map.setCollisionBetween(0,13);
@@ -57,37 +57,39 @@ function create() {
         frameRate: 7,
         frames: this.anims.generateFrameNames("pacman", { start: 0, end: 3 })
     });
-    pacman.play("munch");
+    this.pacman.play("munch");
 
     cursors = this.input.keyboard.createCursorKeys();
 
   //this.dots = this.add.physicsGroup();
-  //this.map.createFromTile(7, this.safetile, "dot", this.layer, this.dot);
-    this.physics.add.collider(pacman, this.layer);
+    this.physics.add.collider(this.pacman, this.layer);
+    //this.pacman.body.setEnable();
+    this.pacman.body.setSize(16,16,true)
+    this.pacman.body.setCollideWorldBounds(true);
 
+    /*
     const debugGraphics = this.add.graphics().setAlpha(0.75);
     this.map.renderDebug(debugGraphics, {
         tileColor: null, // Color of non-colliding tiles
         collidingTileColor: new Phaser.Display.Color(243, 134, 48, 255), // Color of colliding tiles
         faceColor: new Phaser.Display.Color(40, 39, 37, 255) // Color of colliding face edges
     });
+    */
 }
 
 function update() {
-    if (cursors.up.isDown) {
-        pacman.y--;
-        pacman.angle = 270;
-    }
-    if (cursors.down.isDown) {
-        pacman.y++;
-        pacman.angle = 90;
-    }
+
+  // Horizontal movement
     if (cursors.left.isDown) {
-        pacman.x--;
-        pacman.angle = 180;
+        this.pacman.body.setVelocityX(-100);
+    } else if (cursors.right.isDown) {
+        this.pacman.body.setVelocityX(100);
     }
-    if (cursors.right.isDown) {
-        pacman.x++;
-        pacman.angle = 0;
+
+  // Vertical movement
+    if (cursors.up.isDown) {
+        this.pacman.body.setVelocityY(-100);
+    } else if (cursors.down.isDown) {
+        this.pacman.body.setVelocityY(100);
     }
 }
